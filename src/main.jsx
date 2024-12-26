3f27757b7c144ab498f98850ef3412871646fa64
vickyax <vigneshr75021@gmail.com>
2024-12-26 18:44:20 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,9 @@ import App from './App.jsx'
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css'
 import "./responsive.css";
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
-
-const client = import.meta.env.VITE_REACT_APP_googleauth;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={client}>
     <App />
-    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
