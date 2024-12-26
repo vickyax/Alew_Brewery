4bce8e88ea359bdf9cc59c2ce4076209e1013175
vickyax <vigneshr75021@gmail.com>
2024-12-26 19:33:45 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    port: 5173, // Use PORT from environment variable or default to 5173
-    host: true, // Bind to all network interfaces
-  }
+  base: '/'
 })
